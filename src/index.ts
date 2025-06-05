c04bd7a3e5da350296221b1473903944277d3d0d
SaltWood_233 <105980161+SALTWOOD@users.noreply.github.com>
2025-06-06 03:36:54 +0900

    add: 添加基本代码及 Socket.IO

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-// TODO: Implement backend code
+import express from 'express';
+import { DataSource } from 'typeorm';
+import { Server } from 'socket.io';
+import http from 'http';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+const db = new DataSource({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'asashio',
+    entities: []
+});
+
+await db.initialize();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
